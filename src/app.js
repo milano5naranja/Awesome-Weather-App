44ef7ad7932aa3fb08f9856843ff56c71152b07e
milano5naranja <120511247+milano5naranja@users.noreply.github.com>
2023-06-10 14:27:23 +0100

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
 
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+}
+
+let apiKey = "4f0252e46f5ob21t364250ae01f31bc7";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Helsinki&key=${apiKey}`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
