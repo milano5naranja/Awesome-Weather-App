0fd3f8d36325dc83c87140ad26bed516e67f742d
milano5naranja <120511247+milano5naranja@users.noreply.github.com>
2023-06-10 15:11:24 +0100

    updated the hours

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
+  //if (hours < 10) {
+  //hours = `0${hours}`;
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
@@ -21,22 +21,22 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "4f0252e46f5ob21t364250ae01f31bc7";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Helsinki&key=${apiKey}`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
