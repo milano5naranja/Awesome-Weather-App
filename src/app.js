534c043f71af51ef67b88cb6a4f7509cec35928a
milano5naranja <120511247+milano5naranja@users.noreply.github.com>
2023-06-12 12:07:54 +0100

    Added forecast weekdays

--- src/app.js
+++ src/app.js
@@ -20,6 +20,38 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Mon", "Tue", "Wed", "Thu"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+     
+      <div class="col-2">
+      <div class="weather-forecast-date"> ${day}</div>
+      <img
+      src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+      alt"" 
+      width="42" 
+      />
+      
+      <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-temperature-max"> 18° </span>
+      <span class="weather-forecast-temperature-min"> 12°</span>
+      </div>
+      </div>
+    
+      `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -68,3 +100,4 @@ function displayCelciusTemperature(event) {
 }
 
 search("Helsinki");
+displayForecast();
